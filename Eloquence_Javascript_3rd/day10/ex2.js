ed1aa0a5ae9ac9445e4f594b4ab08db4484fd80d
Bui Quoc Huy <t9buqu00@students.oamk.fi>
2020-05-29 05:30:12 +0300

    ex1 chapter 11 adding asyn file

--- Eloquence_Javascript_3rd/day10/ex2.js
+++ Eloquence_Javascript_3rd/day10/ex2.js
@@ -2,12 +2,12 @@
 function Promise_all(promises) {
     return new Promise((resolve, reject) => {
         let results = [];
-        let pending = promises.length;
+        let waiting = promises.length;
         for (let i = 0; i < promises.length; i++) {
             promises[i].then(result => {
                 results[i] = result;
-                pending--;
-                if (pending == 0) resolve(results);
+                waiting--;
+                if (waiting == 0) resolve(results);
             }).catch(reject);
         }
         if (promises.length == 0) resolve(results);
