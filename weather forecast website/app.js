0ad6861d509e7615e458ca7219a431514b55aa03
Bui Quoc Huy <t9buqu00@students.oamk.fi>
2020-08-17 23:29:18 +0300

    Update app.js

--- weather forecast website/app.js
+++ weather forecast website/app.js
@@ -1,11 +1,25 @@
 // Init object 
-const weather = new Weather('Boston', 'US');
+const weather = new Weather('Oulu', 'Finaland');
 
 //Init UI
 const ui = new UI();
 //get weather from on DOM load 
 document.addEventListener('DOMContentLoaded', getWeather());
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
 
+    weather.changeLocation('Miami', 'FL');
+
+    //get and display weather
+
+    getWeather();
+
+    //Close modal
+
+    $('#locModal').modal('hide');
+})
 
 function getWeather() {
     weather.getWeather()
